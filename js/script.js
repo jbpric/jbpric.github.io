fd4a76e7e253387602ca76eb8f2c893111137636
JB PRIC C.A <138726590+jbpric@users.noreply.github.com>
2023-07-07 00:31:20 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
   var header = document.querySelector(".header");
   var content = document.querySelector(".acerca2");
+  var slideIndex = 0;
+
+  showSlides();
+
+function showSlides() {
+  var slides = document.getElementsByClassName("slide");
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].classList.add("active");
+  setTimeout(showSlides, 3500); // Cambiar imagen cada 3 segundos (3000 milisegundos)
+}
 
   function updateHeaderShadow() {
     var rect = content.getBoundingClientRect();
@@ -18,3 +34,5 @@ document.addEventListener("DOMContentLoaded", function() {
   // Llamamos a la función una vez al cargar la página para verificar la posición inicial
   updateHeaderShadow();
 });
+
+
